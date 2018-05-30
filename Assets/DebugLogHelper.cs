77b94e81c8e6e05261023ea3bcd8131836c4ce61
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-05-29 18:09:08 -0700

    Debug stuff

---
+++ Assets/DebugLogHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DebugLogHelper : MonoBehaviour
+{
+    public void DebugLog(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public void DebugLog(float value)
+    {
+        Debug.Log(value);
+    }
+}
