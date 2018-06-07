fe1e48084709b3c3cd7691e47c0f0a7660f83c06
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-06-07 15:38:17 -0700

    Input module with mouse input on/off option (meant to commit this before, oops)

---
+++ Assets/Common/StandaloneInputModule2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.EventSystems;
+
+namespace CVRLabSJSU
+{
+    public class StandaloneInputModule2 : StandaloneInputModule
+    {
+        public bool AllowProcessMouseEvent = true;
+
+        public override void Process()
+        {
+            bool usedEvent = SendUpdateEventToSelectedObject();
+
+            if (eventSystem.sendNavigationEvents)
+            {
+                if (!usedEvent)
+                    usedEvent |= SendMoveEventToSelectedObject();
+
+                if (!usedEvent)
+                    SendSubmitEventToSelectedObject();
+            }
+
+            if (AllowProcessMouseEvent)
+                ProcessMouseEvent();
+        }
+    }
+}
