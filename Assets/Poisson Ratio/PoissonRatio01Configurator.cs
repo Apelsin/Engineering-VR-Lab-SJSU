a4ce3f13ac1a75fbcf7c2b5b03c51ac4a827bf4e
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-06-07 14:41:23 -0700

    Selection highlights!

--- Assets/Poisson Ratio/PoissonRatio01Configurator.cs
+++ Assets/Poisson Ratio/PoissonRatio01Configurator.cs
@@ -1,4 +1,5 @@
-﻿using CVRLabSJSU;
+﻿using cakeslice;
+using CVRLabSJSU;
 using UnityEngine;
 
 public class PoissonRatio01Configurator : MonoBehaviour
@@ -11,6 +12,21 @@ public class PoissonRatio01Configurator : MonoBehaviour
         foreach (var fc in FindObjectsOfType<LookAtCamera>())
             fc.CameraTransform = main_camera_object.transform;
 
+        var pointer_menu_manager = FindObjectOfType<PointerMenuManager>();
 
+        pointer_menu_manager.MenuAdded.AddListener(HandleMenuAdded);
+        pointer_menu_manager.MenuRemoved.AddListener(HandleMenuRemoved);
+    }
+
+    private void HandleMenuAdded(object sender, PointerMenuManager.PointerMenuEventArgs args)
+    {
+        args.RaycastHit.transform.gameObject.AddComponent<Outline>();
+    }
+
+    private void HandleMenuRemoved(object sender, PointerMenuManager.PointerMenuEventArgs args)
+    {
+        var outline = args.RaycastHit.transform.gameObject.GetComponent<Outline>();
+        if (outline)
+            Destroy(outline);
     }
 }
