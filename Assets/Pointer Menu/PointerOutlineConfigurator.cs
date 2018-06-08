1f48b4eec502c0b595c7ffdfcc458acabcb2c32c
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-06-07 17:38:53 -0700

    Separate pointer outline logic

---
+++ Assets/Pointer Menu/PointerOutlineConfigurator.cs
@@ -0,0 +1,26 @@
+﻿using cakeslice;
+using CVRLabSJSU;
+using UnityEngine;
+
+public class PointerOutlineConfigurator : MonoBehaviour
+{
+    private void Start()
+    {
+        var pointer_menu_manager = FindObjectOfType<PointerMenuManager>();
+
+        pointer_menu_manager.MenuAdded.AddListener(HandleMenuAdded);
+        pointer_menu_manager.MenuRemoved.AddListener(HandleMenuRemoved);
+    }
+
+    private void HandleMenuAdded(object sender, PointerMenuManager.PointerMenuEventArgs args)
+    {
+        args.RaycastHit.transform.gameObject.AddComponent<Outline>();
+    }
+
+    private void HandleMenuRemoved(object sender, PointerMenuManager.PointerMenuEventArgs args)
+    {
+        var outline = args.RaycastHit.transform.gameObject.GetComponent<Outline>();
+        if (outline)
+            Destroy(outline);
+    }
+}
--- Assets/Poisson Ratio/PoissonRatio01Configurator.cs
+++ Assets/Poisson Ratio/PoissonRatio01Configurator.cs
@@ -1,32 +1,9 @@
-﻿using cakeslice;
-using CVRLabSJSU;
+﻿using CVRLabSJSU;
 using UnityEngine;
 
 public class PoissonRatio01Configurator : MonoBehaviour
 {
     private void Start()
     {
-        //yield return new WaitForSeconds(1f);
-        var main_camera_object = GameObject.FindGameObjectWithTag("MainCamera");
-        //var main_camera = main_camera_object.GetComponent<Camera>();
-        foreach (var fc in FindObjectsOfType<LookAtCamera>())
-            fc.CameraTransform = main_camera_object.transform;
-
-        var pointer_menu_manager = FindObjectOfType<PointerMenuManager>();
-
-        pointer_menu_manager.MenuAdded.AddListener(HandleMenuAdded);
-        pointer_menu_manager.MenuRemoved.AddListener(HandleMenuRemoved);
-    }
-
-    private void HandleMenuAdded(object sender, PointerMenuManager.PointerMenuEventArgs args)
-    {
-        args.RaycastHit.transform.gameObject.AddComponent<Outline>();
-    }
-
-    private void HandleMenuRemoved(object sender, PointerMenuManager.PointerMenuEventArgs args)
-    {
-        var outline = args.RaycastHit.transform.gameObject.GetComponent<Outline>();
-        if (outline)
-            Destroy(outline);
     }
 }
