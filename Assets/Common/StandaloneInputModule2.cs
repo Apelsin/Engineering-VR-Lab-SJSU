91aa3336a1ea677df1e2c289c6e433924f101ef5
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-06-09 04:02:41 -0700

    Adding this for historical purposes. Will remove later.

--- Assets/Common/StandaloneInputModule2.cs
+++ Assets/Common/StandaloneInputModule2.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine.EventSystems;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CVRLabSJSU
 {
@@ -22,5 +23,29 @@ namespace CVRLabSJSU
             if (AllowProcessMouseEvent)
                 ProcessMouseEvent();
         }
+
+        private static void Execute(IPointerHoverHandler handler, BaseEventData eventData)
+        {
+            var pointer_event_data = ExecuteEvents.ValidateEventData<PointerEventData>(eventData);
+            handler.OnPointerHover(pointer_event_data);
+        }
+
+        public static ExecuteEvents.EventFunction<IPointerHoverHandler> PointerHoverHandler => Execute;
+
+        protected override void ProcessMove(PointerEventData pointerEvent)
+        {
+            base.ProcessMove(pointerEvent);
+            var targetGO = (Cursor.lockState == CursorLockMode.Locked ? null : pointerEvent.pointerCurrentRaycast.gameObject);
+            HandlePointerHover(pointerEvent, targetGO);
+        }
+
+        protected void HandlePointerHover(PointerEventData currentPointerData, GameObject newEnterTarget)
+        {
+            currentPointerData.pointerEnter = newEnterTarget;
+            var hovered = currentPointerData.hovered;
+            for (int i = 0; i < hovered.Count; i++)
+                ExecuteEvents.Execute(hovered[i], currentPointerData, PointerHoverHandler);
+        }
     }
+
 }
