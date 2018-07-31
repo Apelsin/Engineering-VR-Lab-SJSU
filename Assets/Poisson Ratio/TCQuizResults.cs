52565d04967ad9ffaee10d034d920e9e035b2bb3
Vincent Brubaker-Gianakos <appelsindrikken@gmail.com>
2018-07-31 01:35:10 -0700

    Namespace + formatting

--- Assets/Poisson Ratio/TCQuizResults.cs
+++ Assets/Poisson Ratio/TCQuizResults.cs
@@ -1,46 +1,49 @@
 ﻿using cakeslice;
-using CVRLabSJSU;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TCQuizResults : MonoBehaviour
+namespace CVRLabSJSU
 {
-    public TensionCompressionQuiz.TCObjects TensionObjects;
-    public TensionCompressionQuiz.TCObjects CompressionObjects;
-    public int CorrectOutlineColorIndex = 1;
-    public int IncorrectOutlineColorIndex = 2;
-    private List<Outline> Outlines = new List<Outline>();
-    public void HandleDisplayResults(object sender, MCQuizResultsEventArgs args)
+    public class TCQuizResults : MonoBehaviour
     {
-        Clear();
-        MarkCorrect(args.Choices, TensionObjects);
-        MarkCorrect(args.Choices, CompressionObjects);
-    }
+        public TensionCompressionQuiz.TCObjects TensionObjects;
+        public TensionCompressionQuiz.TCObjects CompressionObjects;
+        public int CorrectOutlineColorIndex = 1;
+        public int IncorrectOutlineColorIndex = 2;
+        private List<Outline> Outlines = new List<Outline>();
 
-    private void Clear()
-    {
-        foreach (var outline in Outlines)
-            Destroy(outline);
-        Outlines.Clear();
-    }
-
-    private void MarkCorrect(IReadOnlyDictionary<string, MultipleChoiceQuizItem.Option> choices, TensionCompressionQuiz.TCObjects objects)
-    {
-        foreach (var choice in choices)
+        public void HandleDisplayResults(object sender, MCQuizResultsEventArgs args)
         {
-            var id = choice.Key;
-            var tc_object =
-                (typeof(TensionCompressionQuiz.TCObjects)
-                .GetField(id)?
-                .GetValue(objects) as GameObject);
-            if(tc_object)
+            Clear();
+            MarkCorrect(args.Choices, TensionObjects);
+            MarkCorrect(args.Choices, CompressionObjects);
+        }
+
+        private void Clear()
+        {
+            foreach (var outline in Outlines)
+                Destroy(outline);
+            Outlines.Clear();
+        }
+
+        private void MarkCorrect(IReadOnlyDictionary<string, MultipleChoiceQuizItem.Option> choices, TensionCompressionQuiz.TCObjects objects)
+        {
+            foreach (var choice in choices)
             {
-                var outline = tc_object.AddComponent<Outline>();
-                if (choice.Value.IsCorrect)
-                    outline.color = CorrectOutlineColorIndex;
-                else
-                    outline.color = IncorrectOutlineColorIndex;
-                Outlines.Add(outline);
+                var id = choice.Key;
+                var tc_object =
+                    (typeof(TensionCompressionQuiz.TCObjects)
+                    .GetField(id)?
+                    .GetValue(objects) as GameObject);
+                if (tc_object)
+                {
+                    var outline = tc_object.AddComponent<Outline>();
+                    if (choice.Value.IsCorrect)
+                        outline.color = CorrectOutlineColorIndex;
+                    else
+                        outline.color = IncorrectOutlineColorIndex;
+                    Outlines.Add(outline);
+                }
             }
         }
     }
